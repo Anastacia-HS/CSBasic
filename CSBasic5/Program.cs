7e5ff8c241207723f93c28f0f553bc2ea1d21473
Anastasia <dbwjddbsgk@gmail.com>
2021-07-01 09:41:13 +0900

    #35 클래스 메서드 실습

--- CSBasic5/Program.cs
+++ CSBasic5/Program.cs
@@ -8,8 +8,83 @@ namespace CSBasic5
 {
     class Program
     {
+        public int instanceVariable;
+        public int instanceMethod()
+        {
+            return 0;
+        }
+
+        class MyMath
+        {
+            public static int Abs(int input)
+            {
+                return (input < 0) ? -input : input;
+                /*if(input < 0)
+                {
+                    return -input;
+                }
+                else
+                {
+                    return input;
+                }*/
+            }
+        }
+
+        class Test
+        {
+            // Method Signature
+            public int Sum(int min, int max)
+            {
+                int output = 0;
+                for (int i = min; i <= max; i++)
+                {
+                    output += i;
+                }
+                return output;
+            }
+        }
+
+        class MethodExample
+        {
+            public int Power(int x)
+            {
+                return x * x;
+            }
+
+            public int Multi(int x, int y)
+            {
+                return x * y;
+            }
+
+            public void Print()
+            {
+                Console.WriteLine("Print() 메서드 호출됨");
+            }
+        }
+
         static void Main(string[] args)
         {
+            MethodExample me = new MethodExample();
+            Console.WriteLine(me.Power(10));
+            Console.WriteLine(me.Power(20));
+            Console.WriteLine(me.Multi(52, 273));
+            Console.WriteLine(me.Multi(103, 32));
+            me.Print();
+
+            Test test = new Test();
+            Console.WriteLine(test.Sum(1, 100));
+            
+            Console.WriteLine(MyMath.Abs(50));
+            Console.WriteLine(MyMath.Abs(-273));
+
+            // 클래스 메서드에서는 인스턴스 변수 / 메서드 접근 불가
+            // 객체를 만들어 접근해야한다.
+            // Console.WriteLine(instanceVariable);
+            // instanceMethod();
+
+            Program p = new Program();
+            Console.WriteLine(p.instanceVariable);
+            p.instanceMethod();
         }
     }
 }
